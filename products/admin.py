3b7649821bb9c4956c416eda3ca3d35384778401
Linda2009 <69783444+Linda2009@users.noreply.github.com>
2021-11-30 12:49:53 +0200

    create category and product model and admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
+from mptt.admin import MPTTModelAdmin
+from .models import (Category,Product,)
 
-# Register your models here.
+
+
+
+#_____________________________ CategoryAdmin_______________________________
+class CategoryAdmin(admin.ModelAdmin):
+        search_fields = ['name']
+        readonly_fields = ('my_image_tag',)
+        list_display=['title','status','my_image_tag']
+        list_filter=['status','create_at']
+
+
+
+admin.site.register(Category,MPTTModelAdmin)
+admin.site.register(models.Product)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
+from django.utils.safestring import mark_safe
+        
+class Category(MPTTModel):
+ 
+    name = models.CharField( max_length= 300, db_index=True,verbose_name=_("Category Name"), )
+    slug = models.SlugField(verbose_name=_("Category safe URL"),max_length= 300, unique=True) 
+    parent = TreeForeignKey('self', on_delete=models.CASCADE,null=True, blank=True, related_name='children')
+    cat_image = models.ImageField(upload_to='products/images' , verbose_name=_("Image") , blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    class MPTTMeta:
+        order_insertion_by = ['name']
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")  
+        
+        
+      
+        # display image in admin panel
+    def image_disp(self):
 
-# Create your models here.
+        if self.cat_image:
+            return mark_safe(f'<img src="{self.cat_image.url}" height="50"/>')
+        else:
+            return ""
+    
+    def save(self, *args, **kwargs):
+    
+        self.slug = self.slug()
+        return super(Category, self).save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.name 
+        
+class Product(models.Model):
+    """
+    tuoteiden taulussa sisältä kaikki tuoteitä
+    """
+  
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    name = models.CharField(help_text=_("Required"),max_length=255,verbose_name=_("Product Name "))
+    description = models.TextField(verbose_name=_("description"), help_text=_("Not Required"), blank=True)
+    slug = models.SlugField(verbose_name=_("Product safe URL"), max_length=255)
+    regular_price = models.DecimalField(verbose_name=_("Regular price"), max_digits=20,decimal_places=2)
+    discount_price = models.DecimalField(verbose_name=_("Discount price"),max_digits=20,decimal_places=2)
+    cost = models.DecimalField(max_digits=20 , decimal_places=2 , verbose_name=_("Cost"))
+    prod_image = models.ImageField(
+        verbose_name=_("image"),
+        help_text=_("Upload a product image"),
+        upload_to="products/images",
+        default="images/default.png",
+    )
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(
+        verbose_name=_("Product visibility"),
+        help_text=_("Change product visibility"),
+        default=True,
+    )
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+    PRDISNew = models.BooleanField(default=True , verbose_name=_("New Product "))
+    PRDISBestSeller = models.BooleanField(default=False , verbose_name=_("Best Seller"))
+  
+       
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+    
+    # def get_absolute_url(self):
+    #     return reverse("", args=[self.slug])
+    
+    
+    # ----------------------# display image in admin panel-----------------------
+    def prod_image_disp(self):
+    
+        if self.prod_image:
+            return mark_safe(f'<img src="{self.prod_image.url}" height="50"/>')
+        else:
+            return ""
+    
+
+    # ----------------------save the slug--------------------------------------------
+    def save(self, *args, **kwargs):
+    
+        self.slug = self.slug()
+        return super(Category, self).save(*args, **kwargs)
+        
+        
+        
+    def __str__(self):
+        return self.name        
